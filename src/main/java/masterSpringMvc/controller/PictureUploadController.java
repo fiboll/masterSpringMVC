a383491a394e5244046bef8c77787b2f791fd50c
fiboll <fiboll@o2.pl>
2018-02-14 21:12:27 +0100

    add error handling

--- src/main/java/masterSpringMvc/controller/PictureUploadController.java
+++ src/main/java/masterSpringMvc/controller/PictureUploadController.java
@@ -3,6 +3,7 @@ package masterSpringMvc.controller;
 import masterSpringMvc.config.PictureUploadProperties;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
@@ -11,21 +12,27 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLConnection;
+import java.util.Locale;
 
 @Controller
 @SessionAttributes("picturePath")
 public class PictureUploadController {
 	private final Resource picturesDir;
 	private final Resource anonymousPicture;
+	private final MessageSource messageSource;
 
 	@Autowired
-	public PictureUploadController(PictureUploadProperties uploadProperties) {
+	public PictureUploadController(PictureUploadProperties uploadProperties, MessageSource messageSource) {
 		picturesDir = uploadProperties.getUploadPath();
 		anonymousPicture = uploadProperties.getAnonymousPicture();
+		this.messageSource = messageSource;
+
 	}
 
 	@ModelAttribute("picturePath")
@@ -58,12 +65,21 @@ public class PictureUploadController {
 		IOUtils.copy(picturePath.getInputStream(), response.getOutputStream());
 	}
 
-	@ExceptionHandler(IOException.class)
-	public ModelAndView handleIOException(IOException exception) {
-		ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
-		modelAndView.addObject("error", exception.getMessage());
+	@RequestMapping("uploadError")
+	public ModelAndView onUploadError(HttpServletRequest request, Locale locale) {
+		ModelAndView modelAndView = new ModelAndView("uploadPage");
+		modelAndView.addObject("error",	messageSource.getMessage("upload.file.too.big", null, locale));;
 		return modelAndView;
 	}
+
+	@ExceptionHandler(IOException.class)
+	public ModelAndView handleIOException(IOException exception, Locale locale) {
+		ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+		modelAndView.addObject("error", messageSource.getMessage("upload.io.exception", null, locale));
+		return modelAndView;
+	}
+
+
 
 	private Resource copyFileToPictures(MultipartFile file) throws IOException {
 
