8d94bbaa9526c86c5aa3235d791b41aa5423ce75
fiboll <fiboll@o2.pl>
2018-02-20 13:33:22 +0100

    add security

--- src/main/java/masterSpringMvc/config/SecurityConfiguration.java
+++ src/main/java/masterSpringMvc/config/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package masterSpringMvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -33,5 +37,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.hasRole("ADMIN")
 			.anyRequest().authenticated();
 	}
+	
+//	@Bean
+//	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+//	    final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//	    templateEngine.setTemplateResolver(templateResolver);
+//	    templateEngine.addDialect(sec); // Enable use of "sec"
+//	    return templateEngine;
+//	}
 
 }
